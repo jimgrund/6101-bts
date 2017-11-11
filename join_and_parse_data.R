48a6fa025dffbdfb21db6b86fe0211ef2cf493c3
Imgrund, James <james.imgrund@teamaol.com>
2017-11-11 01:48:58 -0500

    updated with file check to ensure file exists before trying to read in data

--- join_and_parse_data.R
+++ join_and_parse_data.R
@@ -1,5 +1,6 @@
 bts_data <- "data/"
 setwd(bts_data)
+rm(complete_data)
 
 complete_data <- data.frame(Date=as.Date(character()),
                          File=character(), 
@@ -10,13 +11,14 @@ for(Year in 2015:2017) {
   for(Month in 1:12) {
     filename<-(paste(paste("On_Time_On_Time_Performance",Year, Month, sep="_"),".csv",sep=""))
     print(filename)
-    mydata <- read.csv(filename)
-    relevant_data <- subset(mydata, Origin == "IAD" | Dest == "IAD" |
-                              Origin == "BWI" | Dest == "BWI" |
-                              Origin == "DCA" | Dest == "DCA")
+    if (file.exists(filename)) {
+      mydata <- read.csv(filename)
+      relevant_data <- subset(mydata, Origin == "IAD" | Dest == "IAD" |
+                                Origin == "BWI" | Dest == "BWI" |
+                                Origin == "DCA" | Dest == "DCA")
     
-    complete_data <- rbind(complete_data, relevant_data)
-    
-    rm(mydata, relevant_data)
+      complete_data <- rbind(complete_data, relevant_data)
+      rm(mydata, relevant_data)
+    }
   }
 }
