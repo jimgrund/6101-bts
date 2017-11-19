134a0f7bfff6180f5ce7c28773987e718a3b9f58
jassalak <akash.jassal09@gmail.com>
2017-11-19 15:46:13 -0500

    Add files via upload
    Check out the Confusion Matrix... Even though we get a good accuracy, Does not look right,  
    I think it has something to do with factors vs not.

--- teambestteam_proj2.R
+++ teambestteam_proj2.R
@@ -7,6 +7,9 @@
 #'      toc: true
 #'      highlight: haddock
 #' ---
+########################################
+#http://rpubs.com/jassalak/TeamBestTeam_Proj2
+########################################
 #' ## Environment Preparation
 #Remove any objects in the Environment
 #rm(list = ls())
@@ -30,13 +33,23 @@ suppressPackageStartupMessages(library(BSDA))
 suppressPackageStartupMessages(library(MASS))
 #install.packages("Hmisc")
 suppressPackageStartupMessages(library(Hmisc))
+#install.packages("caret")
+suppressPackageStartupMessages(library(caret))
+#install.packages("ROCR")
+suppressPackageStartupMessages(library(ROCR))
+#install.packages("rpart")
+suppressPackageStartupMessages(library(rpart))
+#install.packages("dplyr")
+#suppressPackageStartupMessages(library(dplyr))
+
 
 #' ###  Data Load
 # Load the dataset from the working directory
-#data_all <- data.frame(read.csv("DMV_On_Time_Performance_2015-2017.csv", header = TRUE))
+data_all <- data.frame(read.csv("DMV_On_Time_Performance_2015-2017.csv", header = TRUE))
 
 #' ## Preprocess Data
 #' ### Global Filter Data
+#d1 <- subset(data_all, Year == "2015" | Year =="2016")
 d1 <- subset(data_all, Year == "2015" | Year =="2016" | Year == "2017")
 
 #' ### Missing Values Clean-up
@@ -56,19 +69,93 @@ d1 <- d1[,colSums(is.na(d1)) < (nrow(d1)*0.80)]
 #+ eval = FALSE, echo = TRUE
 colSums(is.na(d1))
 
+#' #### Check for Empty Data
+#+ eval = FALSE, echo = TRUE
+colSums(d1=='')
+
+#' #### Remove Cols if high values of Blanks
+d1 <- subset(d1,select = -c(TailNum, CancellationCode, Div1Airport, Div1TailNum, Div2Airport, Div2TailNum))
+#+ eval = FALSE, echo = TRUE
+colSums(d1=='')
+
+#' #### Check number of uniques values for each of the column
+#+ eval = FALSE, echo = TRUE
+sapply(d1, function(x) length(unique(x)))
+ 
+#' #### Remove Cols if Only One Unique Value Exists
+d1 <- subset(d1,select = -c(Flights))
+
 #' #### Remove Rows if NA exists 
 d1 <- d1[complete.cases(d1), ]
 #+ eval = FALSE, echo = TRUE
 colSums(is.na(d1))
-d1_top20 <- d1[1:20,]
+
+#' Convert Variables to Factor
+#+ eval = FALSE, echo = TRUE
+str(d1)
+#d1$DepDel15 <- factor(d1$DepDel15)
+#levels(d1$DepDel15)
+
+
+#' Top n
+#d1_top20 <- d1[1:20,]
 d1_rand10 <- d1[sample(nrow(d1),10),]
+                
 #' #### Clean up Environment
 rm(data_all)
+########################################
+#' ## Partition Data
+subsamples <- createDataPartition(y=d1$DepDel15, p=0.7, list=FALSE)
+TrainSet <- d1[subsamples, ] 
+TestSet <- d1[-subsamples, ]
+
+rm(subsamples)
+########################################
+#' ## Model 1 (Decision Tree)
+#' 
+#' m1 <- rpart::rpart(DepDel15 ~ ., data=TrainSet, method="class")
+#' printcp(m1)
+#' plotcp(m1)
+#' summary(m1)
+#' plot(m1,uniform=TRUE, main="Classification Tree", extra=102, under=TRUE, faclen=0)
+#' 
+#' ###Prediction
+#' p1 <- predict(m1, TestSet, type = "class")
+#' summary(p1)
+#' plot(p1)
+
+########################################
+#' ## Model 2 (Logistic Regression)
+m2 <- glm(DepDel15 ~ (Year)+(Quarter)+(Month)+(DayofMonth)+DayOfWeek+AirlineID+OriginAirportID+DestAirportID,data=TrainSet) 
+#m2 <- glm(DepDel15 ~ factor(Year)+factor(Quarter)+factor(Month)+factor(DayofMonth)+factor(DayOfWeek)+factor(AirlineID)+factor(OriginAirportID)+factor(DestAirportID),data=TrainSet) 
+summary(m2)
+
+#' ### Using anova() for feature importance
+anova(m2, test="Chisq")
+
+#' ### Prediction
+p2 <- predict(m2, newdata = TestSet,type = 'response')
+p2 <- ifelse(p2 > 0.5,1,0)
+
+#' ### Confusion Matrix to Check Accuracy 
+confusionMatrix(data=p2, reference = TestSet$DepDel15)
+
+#' ### ROC and AUC
+#predictions <- predict(m2, newdata=TestSet, type="response")
+ROCRpred <- prediction(p2, TestSet$DepDel15)
+ROCRperf <- performance(ROCRpred, measure = "tpr", x.measure = "fpr")
+
+#plot(ROCRperf, colorize = TRUE, text.adj = c(-0.2,1.7), print.cutoffs.at = seq(0,1,0.1))
+#plot(ROCRperf, text.adj = c(-0.2,1.7), print.cutoffs.at = seq(0,1,0.1))
+########################################
+
+#https://analyticsdefined.com/implementing-logistic-regression-using-titanic-dataset-r/
+
 
 #' Target Variables -->
 # DepDelay (Includes Negatives)
 # DepDelayMinutes (Same as DepDelay but negs are 0)
-# DepDel15 --> Flag if DepDelay >15
+# DepDel15 --> Flag if DepDelay >= 15
 # DepartureDelayGroups --> Bins DepDelay (-2:12)
 
 #'Questions -->
