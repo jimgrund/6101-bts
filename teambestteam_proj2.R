9d5b76d99a01e4f90cd51755f0b8845c8c94d248
jassalak <akash.jassal09@gmail.com>
2017-11-21 20:21:05 -0500

    11.21.17 --> 20:20

--- teambestteam_proj2.R
+++ teambestteam_proj2.R
@@ -50,7 +50,7 @@ data_all <- data.frame(read.csv("DMV_On_Time_Performance_2015-2017.csv", header 
 
 #' ## Preprocess Data
 #' ### Global Filter Data
-d1 <- subset(data_all, Year =="2016")
+d1 <- subset(data_all, Year =="2016" & Origin =="IAD" | Origin =="BWI" | Origin =="DCA")
 #d1 <- subset(data_all, Year == "2015" | Year =="2016" | Year == "2017")
 
 #' ### Missing Values Clean-up
@@ -103,7 +103,7 @@ d1_rand10 <- d1[sample(nrow(d1),10),]
 rm(data_all)
 ########################################
 #' ## Create Model DataFrame
-d2 <- subset(d1,select = c(DepDel15,Year,Quarter,Month,DayofMonth,DayOfWeek,AirlineID,OriginAirportID,DestAirportID))
+d2 <- subset(d1,select = c(DepartureDelayGroups,Year,Quarter,Month,DayofMonth,DayOfWeek,AirlineID,OriginAirportID,DestAirportID))
 d2_rand10 <- d2[sample(nrow(d2),10),]
 
 #' ### Convert Variables to Factor
@@ -112,62 +112,69 @@ str(d2)
 #d2$DepDel15 <- factor(d2$DepDel15)
 #levels(d2$DepDel15)
 #cols <- c("AirlineID","OriginAirportID","DestAirportID")
-#cols <- c("Year","Quarter","Month","DayofMonth","DayOfWeek","AirlineID","OriginAirportID","DestAirportID")
-#d2[cols] <- lapply(d2[cols], factor)
+cols <- c("Year","Quarter","Month","DayofMonth","DayOfWeek","AirlineID","OriginAirportID","DestAirportID")
+d2[cols] <- lapply(d2[cols], factor)
 #+ eval = FALSE, echo = TRUE
 sapply(d2,class)
 
 #' ## Partition Data
-subsamples <- createDataPartition(y=d2$DepDel15, p=0.7, list=FALSE)
+subsamples <- createDataPartition(y=d2$DepartureDelayGroups, p=0.7, list=FALSE)
 TrainSet <- d2[subsamples, ] 
 TestSet <- d2[-subsamples, ]
 rm(subsamples)
+
+hist(d2$DepartureDelayGroups)
+
+#Baseline % -->
+length(d2$DepartureDelayGroups[d2$DepartureDelayGroups==1]) / nrow(d2) * 100
 ########################################
-#' ## Model 1 (Decision Tree)
-#' 
-#' m1 <- rpart::rpart(DepDel15 ~ ., data=TrainSet, method="class")
-#' printcp(m1)
-#' plotcp(m1)
-#' summary(m1)
-#' plot(m1,uniform=TRUE, main="Classification Tree", extra=102, under=TRUE, faclen=0)
-#' 
-#' ###Prediction
-#' p1 <- predict(m1, TestSet, type = "class")
-#' summary(p1)
-#' plot(p1)
+## Model 1 (Decision Tree)
+
+m1 <- rpart(DepartureDelayGroups ~ AirlineID+OriginAirportID+DestAirportID, data=TrainSet, method="class")
+printcp(m1)
+plotcp(m1)
+summary(m1)
+plot(m1,uniform=TRUE, main="Classification Tree", extra=102, under=TRUE, faclen=0)
+
+###Prediction
+p1 <- predict(m1, TestSet, type = "class")
+summary(p1)
+plot(p1)
 
 ########################################
-#' ## Model 2 (Logistic Regression)
-
-#xtabs(~DepDel15 + AirlineID, data = d1)
-
-#' MODEL
-#m2 <- glm(DepDel15 ~ AirlineID+OriginAirportID+DestAirportID,data=TrainSet) 
-m2 <- glm(DepDel15 ~ (Year)+(Quarter)+(Month)+(DayofMonth)+DayOfWeek+AirlineID+OriginAirportID+DestAirportID,data=TrainSet, family=binomial(link = "logit")) 
-#m2 <- glm(DepDel15 ~.,data = TrainSet, family=binomial(link = "logit"))
-#m2 <- glm(DepDel15 ~ factor(Year)+factor(Quarter)+factor(Month)+factor(DayofMonth)+factor(DayOfWeek)+factor(AirlineID)+factor(OriginAirportID)+factor(DestAirportID),data=TrainSet) 
-summary(m2)
-
-#' ### Using anova() for feature importance
-#anova(m2, test="Chisq")
-
-#' ### Prediction
-p2 <- predict(m2, newdata = TestSet,type = 'response')
-p2 <- ifelse(p2 > 0.5,1,0)
-
-#' ### Confusion Matrix to Check Accuracy 
-confusionMatrix(data=p2, reference = TestSet$DepDel15)
-
-#' ### ROC and AUC
-response_predict <- predict(m2, newdata=TestSet, type = "response")
-link_predict <- predict(m2, newdata=TestSet, type = "link")
-qplot(x=response_predict, geom="histogram")
-#predictions <- predict(m2, newdata=TestSet, type="response")
-ROCRpred <- prediction(response_predict, TestSet$DepDel15)
-ROCRperf <- performance(ROCRpred, measure = "tpr", x.measure = "fpr")
-
-plot(ROCRperf, colorize = TRUE, text.adj = c(-0.2,1.7)) #print.cutoffs.at = seq(0,1,0.1))
-#plot(ROCRperf, text.adj = c(-0.2,1.7), print.cutoffs.at = seq(0,1,0.1))
+#' #' ## Model 2 (Logistic Regression)
+#' 
+#' #xtabs(~DepDel15 + AirlineID, data = d1)
+#' 
+#' #' MODEL
+#' #m2 <- glm(DepDel15 ~ AirlineID+OriginAirportID+DestAirportID,data=TrainSet) 
+#' #m2 <- glm(DepDel15 ~ (Quarter)+(Month)+(AirlineID+OriginAirportID+DestAirportID,data=TrainSet, family=binomial(link = "logit")) 
+#' #m2 <- glm(DepDel15 ~.,data = TrainSet, family=binomial(link = "logit"))
+#' #m2 <- glm(DepDel15 ~ factor(Year)+factor(Quarter)+factor(Month)+factor(DayofMonth)+factor(DayOfWeek)+factor(AirlineID)+factor(OriginAirportID)+factor(DestAirportID),data=TrainSet) 
+#' summary(m2)
+#' 
+#' #' ### Using anova() for feature importance
+#' #anova(m2, test="Chisq")
+#' 
+#' #' ### Prediction
+#' p2 <- predict(m2, newdata = TestSet,type = 'response')
+#' p2 <- ifelse(p2 > 0.5,1,0)
+#' str(p2)
+#' p2
+#' 
+#' #' ### Confusion Matrix to Check Accuracy 
+#' confusionMatrix(data=p2, reference = TestSet$DepDel15)
+#' 
+#' #' ### ROC and AUC
+#' response_predict <- predict(m2, newdata=TestSet, type = "response")
+#' link_predict <- predict(m2, newdata=TestSet, type = "link")
+#' qplot(x=response_predict, geom="histogram")
+#' #predictions <- predict(m2, newdata=TestSet, type="response")
+#' ROCRpred <- prediction(response_predict, TestSet$DepDel15)
+#' ROCRperf <- performance(ROCRpred, measure = "tpr", x.measure = "fpr")
+#' 
+#' plot(ROCRperf, colorize = TRUE, text.adj = c(-0.2,1.7)) #print.cutoffs.at = seq(0,1,0.1))
+#' #plot(ROCRperf, text.adj = c(-0.2,1.7), print.cutoffs.at = seq(0,1,0.1))
 ########################################
 
 #https://analyticsdefined.com/implementing-logistic-regression-using-titanic-dataset-r/
@@ -184,3 +191,6 @@ plot(ROCRperf, colorize = TRUE, text.adj = c(-0.2,1.7)) #print.cutoffs.at = seq(
 # We could focus on DepDep15 --> and use Logistic Regression too see what variables are most important...or predict whether or not a flight will be delayed
 # Does Distance affect delay?
 # Is there a difference in Delays by Airport? Is it signif?
+
+# KAPPA Metric --> hurts your model if you are predicting correctly based on chance 
+# if P =! 0 in confusion matrix --> that means that is your baselinemodel (the acc is what you are trying to beat)
