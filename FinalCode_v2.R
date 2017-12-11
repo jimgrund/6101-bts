d90817d8d3f1393becd8addfaf151bb5daa83572
jimgrund <jim.imgrund@gmail.com>
2017-12-11 11:08:58 -0500

    cleaned up
    cleaned up the mess a bit and commented throughout

--- FinalCode_v2.R
+++ FinalCode_v2.R
@@ -28,6 +28,8 @@ opts_chunk$set(eval = TRUE, echo = TRUE, warning = FALSE,
 ### Set the overall seed for reproducibility
 set.seed(6101)
 
+
+
 #'#######################################
 #'
 #' ### Load Necessary Libraries
@@ -53,6 +55,10 @@ suppressPackageStartupMessages(library(pROC))
 
 #'#######################################
 
+
+###
+# read in data that has been filtered down to only DCA, IAD, BWI airports
+
 data <- data.frame(read.csv("DMV_On_Time_Performance_2015-2017.csv", header = TRUE))
 d2 <- subset(data,select = c(Year,
                              Quarter,       #smote this
@@ -69,72 +75,83 @@ d2 <- subset(data,select = c(Year,
                              Dest,          #smote this
                              DestAirportID
 ))
+
+###
+# parse this down to only where DCA, IAD, BWI are the origin airports
 d2.1 <- subset(d2, Origin == "IAD" | Origin == "BWI" | Origin == "DCA")
+
+
+###
+# write this out to a new CSV
 write.csv(d2.1,"DMV-origin_On_Time_Performance_2015-2017.csv")
+
+
+# remove the old dataframes to save on memory consumption
 rm(data,d2,d2.1)
 
-d2.1 <- data.frame(read.csv("DMV-origin_On_Time_Performance_2015-2017.csv", header = TRUE))
 
-row.has.na <- apply(d2.1, 1, function(x){any(is.na(x))})
-d2.1 <- d2.1[!row.has.na,]
+###
+# read in the DVM origin data into a data frame for processing
+d2 <- data.frame(read.csv("DMV-origin_On_Time_Performance_2015-2017.csv", header = TRUE))
+
+
+###
+# remove any nulls and na data from the dataframe
+row.has.na <- apply(d2, 1, function(x){any(is.na(x))})
+d2 <- d2[!row.has.na,]
 rm(row.has.na)
-d2.1 <- d2.1[!apply(is.na(d2.1) | d2.1 == "", 1, all),]
-gc()
-colors = rainbow(length(unique(d2)))
-barchart(d2.1$Origin, ylab="Name of Airport", main="Barchart of Airport Name Frequency (pre-smote)", col=rainbow(3), cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
+d2 <- d2[!apply(is.na(d2) | d2 == "", 1, all),]
 
-bartable <- table(d2.1$DepDel15, d2.1$Origin)
+
+###
+# perform some garbage collection
+gc()
+
+
+
+###
+# prior to any further processing
+# display some graphics of the relation between Departure Delay and some of the key independent variables that we’re looking at
+
+colors = rainbow(length(unique(d2)))
+barchart(d2$Origin, ylab="Name of Airport", main="Barchart of Airport Name Frequency (pre-smote)", col=rainbow(3), cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
+
+bartable <- table(d2$DepDel15, d2$Origin)
 barplot(bartable,xlab="Name of Airport", ylab="Frequency", main="Stacked barchart of Airport Name Frequency vs DepDelay15 (pre-smote)", col = c("Green4","Blue4"), legend = rownames(bartable), cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
 
-bartable <- table(d2.1$DepDel15, d2.1$DayOfWeek)
+bartable <- table(d2$DepDel15, d2$DayOfWeek)
 barplot(bartable,xlab="Day Of Week", ylab="Frequency", main="Day Of Week vs DepDelay15 (pre-smote)", col = c("Green2","Blue2"),  cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
 
-bartable <- table(d2.1$DepDel15, d2.1$Month)
+bartable <- table(d2$DepDel15, d2$Month)
 barplot(bartable,xlab="Month", ylab="Frequency", main="Month vs DepDelay15 (pre-smote)", col = c("Green2","Blue2"),  cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
 
-bartable <- table(d2.1$DepDel15, d2.1$Carrier)
+bartable <- table(d2$DepDel15, d2$Carrier)
 barplot(bartable,xlab="Carrier", ylab="Frequency", main="Carrier vs DepDelay15 (pre-smote)", col = c("Green3","Blue3"),  cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
 
-bartable <- table(d2.1$DepDel15, d2.1$DayofMonth)
+bartable <- table(d2$DepDel15, d2$DayofMonth)
 barplot(bartable,xlab="Day of Month", ylab="Frequency", main="Day of Month vs DepDelay15 (pre-smote)", col = c("Green2","Blue2"),  cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
 
 
-hist(d2.1$DepDel15,xlab="Delay Flag", breaks=10, main="Histogram of Delay Flag Frequency", col = c("Gold3","Brown1"), cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
+hist(d2$DepDel15,xlab="Delay Flag", breaks=10, main="Histogram of Delay Flag Frequency", col = c("Gold3","Brown1"), cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
 
 
 
-#Knitr global options
-library(knitr)
-opts_chunk$set(eval = TRUE, echo = TRUE, warning = FALSE,
-               tidy = TRUE, results = "hold", cache = TRUE)
-
-#Set the overall seed for reproducibility
-set.seed(6101)
 
 
-#d2$DepDel15 <- as.numeric(d2$DepDel15)
 
 #' ## Partition Data
+# create a 70% training set, 30% test set
 subsamples <- createDataPartition(y=d2$DepDel15, p=0.7, list=FALSE)
 TrainSet <- d2[subsamples, ]
 TestSet <- d2[-subsamples, ]
 rm(subsamples)
 rm(d2)
 
+
 #hist(TrainSet$DepDel15)
 
-#TrainSet$DepDel15   <- as.factor(TrainSet$DepDel15)
-#TrainSet$DayOfWeek  <- as.factor(TrainSet$DayOfWeek)
-#TrainSet$DayofMonth <- as.factor(TrainSet$DayofMonth)
-#TrainSet$Month      <- as.factor(TrainSet$Month)
-#TrainSet$Quarter    <- as.factor(TrainSet$Quarter)
-#TrainSet$Carrier    <- as.factor(TrainSet$Carrier)
 
-#FactoredVariable = factor(df$Any)
-#dumm = as.data.frame(model.matrix(~TrainSet$DayOfWeek)[,-1])
-#dfWithDummies = cbind(TrainSet, dumm)
-#str(dfWithDummies)
-
+#’ ##  One-hot-encode the training set
 library(ade4)
 library(data.table)
 ohe_feats = c('DayOfWeek','DayofMonth','Month','Quarter','Carrier')
@@ -145,125 +162,36 @@ for (f in ohe_feats){
 }
 rm(df_all_dummy,ohe_feats)
 
-#str(TrainSet)
-#quit()
-# $ Year                : int  2015 2015 2015 2015 2015 2015 2015 2015 2015 2015 ...
-# $ AirlineID           : int  19805 19805 19805 19805 19805 19805 19805 19805 19805 19805 ...
-# $ Origin              : Factor w/ 98 levels "ABQ","ACK","ALB",..: 24 24 24 24 24 24 24 24 24 24 ...
-# $ Distance            : int  1192 1192 1192 1192 1192 1192 1192 1192 1192 1192 ...
-# $ DepDel15            : int  0 1 0 0 0 1 0 0 0 1 ...
-# $ DepartureDelayGroups: int  0 2 -1 -1 0 4 0 0 -1 1 ...
-# $ DepDelay            : int  9 30 -4 -5 8 61 3 2 -2 25 ...
-# $ Dest                : Factor w/ 99 levels "ABQ","ACK","ALB",..: 26 26 26 26 26 26 26 26 26 26 ...
-# $ DestAirportID       : int  11298 11298 11298 11298 11298 11298 11298 11298 11298 11298 ...
 
+#’ SMOTE the data
+###
+# SMOTE wants the dependent variable to be a factor
 TrainSet$DepDel15 <- as.factor(TrainSet$DepDel15)
 
+# Use Smote to increase the minority to be better balanced with the majority.
+# use only the key independent variables that we care about 
+# in this execution, we remove the ones we don’t care about (notice the usage of the minus sign) rather than listing all the ones we want.
 #newData <- SMOTE(DepDel15 ~ . - Year - AirlineID - Distance - DepartureDelayGroups - DepDelay - DestAirportID, TrainSet, perc.over = 100,perc.under=200, k=7)
-#write.csv(newData,"smoted-one-hot-test.csv")
+
+# write the smoked data out to disk so we can read it in later without having to re-run smote
+#write.csv(newData,"smoted-one-hot-trainset.csv")
 #prop.table(table(newData$DepDel15))
 
 
 
 
-##################################
-##################################
-# model starts here
-##################################
-##################################
+#’ Model
+###
+# Now that we’ve got all the data one-hot-encoded, and training data has been smote’d, 
+# let’s clean up the environment and 
+# let’s run the modeling
 
 
-#' ---
-#' title: 6101 Project 2 b
-#' author: TeamBestTeam
-#' date: 31Oct17
-#' output:
-#'    html_document:
-#'      toc: true
-#'      highlight: haddock
-#' ---
-########################################
-#http://rpubs.com/jassalak/TeamBestTeam_Proj2
-########################################
-#' ## Environment Preparation
+#' ## Model Environment Preparation
 #Remove any objects in the Environment
 rm(list = ls())
 
-#Set working directory
-#setwd("C:/Users/akash/Desktop/GWU/6101_DataScience_RShah/Proj_2")
-setwd("/Users/jimgrund/6101-bts/data/")
 
-
-#Knitr global options
-library(knitr)
-opts_chunk$set(eval = TRUE, echo = TRUE, warning = FALSE,
-               tidy = TRUE, results = "hold", cache = TRUE)
-
-#Set the overall seed for reproducibility
-set.seed(6101)
-
-#' ### Load Necessary Libraries
-
-#install.packages("BSDA")
-suppressPackageStartupMessages(library(BSDA))
-#install.packages("MASS")
-suppressPackageStartupMessages(library(MASS))
-#install.packages("Hmisc")
-suppressPackageStartupMessages(library(Hmisc))
-#install.packages("caret")
-suppressPackageStartupMessages(library(caret))
-#install.packages("ROCR")
-suppressPackageStartupMessages(library(ROCR))
-#install.packages("rpart")
-suppressPackageStartupMessages(library(rpart))
-#install.packages("dplyr")
-#suppressPackageStartupMessages(library(dplyr))
-#install.packages("Amelia")
-#suppressPackageStartupMessages(library(Amelia))
-#install.packages("openxlsx")
-suppressPackageStartupMessages(library(openxlsx))
-#install.packages("party")
-#library(party)
-library(pROC)
-
-
-
-
-########################################
-#' Load Original Data to grab the Test Data
-#Original_df <- data.frame(read.csv("DMV_On_Time_Performance_2015-2017.csv", header = TRUE))
-#Purged_Original_df <- subset(Original_df,select = c(Year,
-#                             Quarter,
-#                             Month,
-#                             DayofMonth,
-#                             DayOfWeek,
-#                             AirlineID,
-#                             Origin,
-#                             Distance,
-#                             DepDel15,
-#                             DepartureDelayGroups,
-#                             DepDelay,
-#                             Carrier,
-#                             Dest,
-#                             DestAirportID
-#                             ))
-#Purged_Original_df <- subset(Purged_Original_df, Origin == "IAD" | Origin == "BWI" | Origin == "DCA")
-#
-#row.has.na <- apply(Purged_Original_df, 1, function(x){any(is.na(x))})
-#Purged_Original_df <- Purged_Original_df[!row.has.na,]
-#rm(row.has.na)
-#
-## compute baseline
-#sum(Purged_Original_df$DepDel15)/length(Purged_Original_df$DepDel15) *100
-#
-##Knitr global options
-#library(knitr)
-#opts_chunk$set(eval = TRUE, echo = TRUE, warning = FALSE,
-#               tidy = TRUE, results = "hold", cache = TRUE)
-#
-##Set the overall seed for reproducibility
-#set.seed(6101)
-#
 
 ########################################
 #' Load Test Data
@@ -271,19 +199,21 @@ TestSet <- data.frame(read.csv("TestSet-OneHot.csv", header = TRUE))
 
 ########################################
 #' Load Smoted Data .. this will be the training data
-d3 <- data.frame(read.csv("smoted-one-hot-test.csv", header = TRUE))
+d3 <- data.frame(read.csv("smoted-one-hot-trainset.csv", header = TRUE))
+TrainSet <- d3
 
 #View(d3)
 #' Create Random Sample
 d3_rand15 <- d3[sample(nrow(d3),15),]
 
-########################################
 
-#' ### Convert Variables to Factor
-#cols <- c("Year","Quarter","Month","DayofMonth","DayofWeek","AirlineID", "Origin")
-#d3[cols] <- lapply(d3[cols], factor)
-##+ eval = FALSE, echo = TRUE
-#sapply(d3,class)
+
+########################################
+#' ## Model 5 (Logistic Regression)
+# join the smoted data with the test test to create a complete dataset 
+# for running the baseline model against
+finalModel<-rbind(d3,TestSet)
+
 
 ########################################
 
@@ -295,33 +225,17 @@ d3_rand15 <- d3[sample(nrow(d3),15),]
 
 #+ echo = FALSE, fig.width=4, fig.height=4, dpi=100
 
-
-########################################
-
-#' ## Partition Data
-#subsamples <- createDataPartition(y=d3$DepDel15, p=0.7, list=FALSE)
-#TrainSet <- d3[subsamples, ]
-TrainSet <- d3
-#TestSet <- d3[-subsamples, ]
-#rm(subsamples)
-
-########################################
-#' ## Model 5 (Logistic Regression)
-# join the smoted data with the test test to create a complete dataset 
-# for running the baseline model against
-finalModel<-rbind(d3,TestSet)
 sum(finalModel$DepDel15)/length(finalModel$DepDel15) *100
 
 
-row.has.na <- apply(TrainSet, 1, function(x){any(is.na(x))})
-TrainSet <- TrainSet[!row.has.na,]
-rm(row.has.na)
 
 #####################################
 #' EDA diagrams
-colors = rainbow(length(unique(TrainSet)))
-barchart(TrainSet$Origin, ylab="Name of Airport", main="Barchart of Airport Name Frequency (post-smote)", col=rainbow(3), cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
+colors = rainbow(length(unique(finalModel)))
+# Airport frequency
+barchart(finalModel$Origin, ylab="Name of Airport", main="Barchart of Airport Name Frequency (post-smote)", col=rainbow(3), cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
 
+# Airport on-time and delay stacked barchart
 bartable <- table(finalModel$DepDel15, finalModel$Origin)
 barplot(bartable,xlab="Name of Airport", ylab="Frequency", main="Stacked barchart of Airport Name Frequency vs DepDelay15 (post-smote)", col = c("Green4","Blue4"), legend = rownames(bartable), cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
 
@@ -333,35 +247,26 @@ hist(finalModel$DepDel15,xlab="Delay Flag", breaks=10, main="Histogram of Delay 
 
 barchart(finalModel$Origin,ylab="Name of Airport", main="Barchart of Airport Name Frequency", col = rainbow(3), cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
 
+
 #bartable <-- table(finalModel$DepDel15, finalModel$Origin)
 #barplot(bartable,ylab="Name of Airport", main="Stacked barchart of Airport Name Frequency vs DepDelay15",
 #        col = c("Green4","Blue4"), legend = rownames(bartable), cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
 
 
-#barchart(finalModel$DepDel15,finalModel$DayOfWeek.1)
-#Monday <- finalModel$DayOfWeek.1[finalModel$DayOfWeek.1==1 & finalModel$DepDel15==1]
-#Tuesday <- finalModel$DayOfWeek.2[finalModel$DayOfWeek.2==1 & finalModel$DepDel15==1]
-#Wednesday <- finalModel$DayOfWeek.3[finalModel$DayOfWeek.3==1 & finalModel$DepDel15==1]
-#DaysOfWeek <- data.frame(length(Monday), length(Tuesday), length(Wednesday))
-#rm(barftable)
-#barftable <-- table(Monday)
-#barplot(barftable,ylab="Name of Airport", main="delay vs day of week",
-#        col = c("Green4","Blue4"), legend = rownames(bartable), cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
-#plot(DaysOfWeek)
-
-
 
 
 #' MODEL
-# remove AirlineID, Distance, DepartureDelayGroups, DepDelay, DestAirportID from the model
+# remove Year, Quarter AirlineID, Distance, DepartureDelayGroups, DepDelay, DestAirportID from the model
 # all other fields from the TrainSet are fair game for use in the model
 m5 <- glm(DepDel15 ~ . - Year - Quarter.1 - Quarter.2 - Quarter.3 - Quarter.4 - AirlineID - Distance - DepartureDelayGroups - DepDelay - DestAirportID, family=binomial(link = "logit"), data=TrainSet)
 summary(m5)
 
+
 #' ### Using anova() for feature importance
 #anova(m5, test="Chisq")
 
-# remove TTN and MSN from TestSet since not found in TrainSet
+
+# remove TTN and MSN from TestSet since not found in TrainSet, else testing the test set against the trainset will fail
 TestSet<-TestSet[!(TestSet$Dest=="TTN"),]
 TestSet<-TestSet[!(TestSet$Dest=="MSN"),]
 
@@ -392,6 +297,8 @@ plot(ROCRperf, colorize = TRUE, text.adj = c(-0.2,1.7),print.cutoffs.at = seq(0,
 ################################
 #' ### Custom Prediction
 options(warn=-1)
+
+# create a customQuery dataframe with defaults to zero for all the one-hot-encoded independent vars
 rm(customQuery)
 customQuery <- data.frame(matrix(ncol = 77, nrow = 0))
 cols <- c("X", "Year", "AirlineID", "Origin", "Distance", "DepDel15", "DepartureDelayGroups", "DepDelay", "Dest", "DestAirportID", "DayOfWeek.1", "DayOfWeek.2", "DayOfWeek.3", "DayOfWeek.4", "DayOfWeek.5", "DayOfWeek.6", "DayOfWeek.7", "DayofMonth.1", "DayofMonth.2", "DayofMonth.3", "DayofMonth.4", "DayofMonth.5", "DayofMonth.6", "DayofMonth.7", "DayofMonth.8", "DayofMonth.9", "DayofMonth.10", "DayofMonth.11", "DayofMonth.12", "DayofMonth.13", "DayofMonth.14", "DayofMonth.15", "DayofMonth.16", "DayofMonth.17", "DayofMonth.18", "DayofMonth.19", "DayofMonth.20", "DayofMonth.21", "DayofMonth.22", "DayofMonth.23", "DayofMonth.24", "DayofMonth.25", "DayofMonth.26", "DayofMonth.27", "DayofMonth.28", "DayofMonth.29", "DayofMonth.30", "DayofMonth.31", "Month.1", "Month.2", "Month.3", "Month.4", "Month.5", "Month.6", "Month.7", "Month.8", "Month.9", "Month.10", "Month.11", "Month.12", "Quarter.1", "Quarter.2", "Quarter.3", "Quarter.4", "Carrier.AA", "Carrier.AS", "Carrier.B6", "Carrier.DL", "Carrier.EV", "Carrier.F9", "Carrier.MQ", "Carrier.NK", "Carrier.OO", "Carrier.UA", "Carrier.US", "Carrier.VX", "Carrier.WN")
@@ -476,6 +383,7 @@ customQuery <- data.frame(X=0,
                           Carrier.WN=0
 )
 
+
 ##########
 #' populate your custom query attributes here
 customQuery$Origin="IAD"
@@ -488,9 +396,6 @@ customQuery$Month.12=1
 customQuery$Carrier.DL=1
 
 
-
+# execute the custom prediction request
 predict(m5,newdata=customQuery,type = "response")
-
-
-
 
