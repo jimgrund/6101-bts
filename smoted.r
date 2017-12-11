9f7becf90cf7d675d7b70b7a33ce7171dd01d7a8
jimgrund <jim.imgrund@gmail.com>
2017-12-10 20:33:25 -0500

    updated to 50-50

--- smoted.r
+++ smoted.r
@@ -38,12 +38,24 @@ rm(list = ls())
 setwd("/Users/jimgrund/6101-bts/data/")
 
 data <- data.frame(read.csv("DMV_On_Time_Performance_2015-2017.csv", header = TRUE))
-d2 <- subset(data,select = c(Year,Quarter,Month,DayofMonth,DayOfWeek,AirlineID,Origin,Distance,DepDel15))
-d2 <- subset(d2, Origin == "IAD" | 
-                 Origin == "BWI" |
-                 Origin == "DCA")
-                 
-rm(data)
+d2 <- subset(data,select = c(Year,
+                             Quarter,       #smote this
+                             Month,         #smote this
+                             DayofMonth,    #smote this
+                             DayOfWeek,     #smote this
+                             AirlineID,
+                             Origin,        #smote this
+                             Distance,
+                             DepDel15,      #smote this
+                             DepartureDelayGroups,
+                             DepDelay,
+                             Carrier,       #smote this
+                             Dest,          #smote this
+                             DestAirportID
+                             ))
+d2 <- subset(d2, Origin == "IAD" | Origin == "BWI" | Origin == "DCA")
+
+#rm(data)
 
 row.has.na <- apply(d2, 1, function(x){any(is.na(x))})
 d2 <- d2[!row.has.na,]
@@ -51,32 +63,61 @@ rm(row.has.na)
 
 #Knitr global options
 library(knitr)
-opts_chunk$set(eval = TRUE, echo = TRUE, warning = FALSE, 
+opts_chunk$set(eval = TRUE, echo = TRUE, warning = FALSE,
                tidy = TRUE, results = "hold", cache = TRUE)
 
 #Set the overall seed for reproducibility
 set.seed(6101)
 
+
+#d2$DepDel15 <- as.numeric(d2$DepDel15)
+
 #' ## Partition Data
-subsamples <- createDataPartition(y=d2$DepDel15, p=0.3, list=FALSE)
-TrainSet <- d2[subsamples, ] 
+subsamples <- createDataPartition(y=d2$DepDel15, p=0.7, list=FALSE)
+TrainSet <- d2[subsamples, ]
 TestSet <- d2[-subsamples, ]
 rm(subsamples)
+rm(d2)
 
-hist(TrainSet$DepDel15)
+#hist(TrainSet$DepDel15)
 
-# look at the data prior to smote
+#TrainSet$DepDel15   <- as.factor(TrainSet$DepDel15)
+#TrainSet$DayOfWeek  <- as.factor(TrainSet$DayOfWeek)
+#TrainSet$DayofMonth <- as.factor(TrainSet$DayofMonth)
+#TrainSet$Month      <- as.factor(TrainSet$Month)
+#TrainSet$Quarter    <- as.factor(TrainSet$Quarter)
+#TrainSet$Carrier    <- as.factor(TrainSet$Carrier)
+
+#FactoredVariable = factor(df$Any)
+#dumm = as.data.frame(model.matrix(~TrainSet$DayOfWeek)[,-1])
+#dfWithDummies = cbind(TrainSet, dumm)
+#str(dfWithDummies)
+
+library(ade4)
+library(data.table)
+ohe_feats = c('DayOfWeek','DayofMonth','Month','Quarter','Carrier')
+for (f in ohe_feats){
+  df_all_dummy = acm.disjonctif(TrainSet[f])
+  TrainSet[f] = NULL
+  TrainSet = cbind(TrainSet, df_all_dummy)
+}
+rm(df_all_dummy,ohe_feats)
+
+#str(TrainSet)
+#quit()
+# $ Year                : int  2015 2015 2015 2015 2015 2015 2015 2015 2015 2015 ...
+# $ AirlineID           : int  19805 19805 19805 19805 19805 19805 19805 19805 19805 19805 ...
+# $ Origin              : Factor w/ 98 levels "ABQ","ACK","ALB",..: 24 24 24 24 24 24 24 24 24 24 ...
+# $ Distance            : int  1192 1192 1192 1192 1192 1192 1192 1192 1192 1192 ...
+# $ DepDel15            : int  0 1 0 0 0 1 0 0 0 1 ...
+# $ DepartureDelayGroups: int  0 2 -1 -1 0 4 0 0 -1 1 ...
+# $ DepDelay            : int  9 30 -4 -5 8 61 3 2 -2 25 ...
+# $ Dest                : Factor w/ 99 levels "ABQ","ACK","ALB",..: 26 26 26 26 26 26 26 26 26 26 ...
+# $ DestAirportID       : int  11298 11298 11298 11298 11298 11298 11298 11298 11298 11298 ...
+
 TrainSet$DepDel15 <- as.factor(TrainSet$DepDel15)
-table(TrainSet$DepDel15)
 
-newData <- SMOTE(DepDel15 ~ ., TrainSet, perc.over = 400,perc.under=100, k=3)
-
-# convert DepDel15 back to numeric
-# unsure why, but smote appears to be converting 0 and 1 to 1 and 2 for DepDel15 /shrug
-newData$DepDel15 <- as.numeric(newData$DepDel15)
-newData$DepDel15 <- (newData$DepDel15-1)
-
-# look at data after smote
-hist(newData$DepDel15)
-table(newData$DepDel15)
-
+#newData <- SMOTE(DepDel15 ~ Quarter+Month+DayofMonth+DayOfWeek+Carrier+Origin+Dest, TrainSet, perc.over = 260,perc.under=100, k=7)
+newData <- SMOTE(DepDel15 ~ . - Year - AirlineID - Distance - DepartureDelayGroups - DepDelay - DestAirportID, TrainSet, perc.over = 100,perc.under=200, k=7)
+write.csv(newData,"smoted-one-hot-test.csv")
+prop.table(table(newData$DepDel15))
